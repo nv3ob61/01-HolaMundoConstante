871b99b0a1a25b8621012766856f23f9ca1ebcca
nv3ob61 <nv3bo61@gmail.com>
2019-10-10 21:55:04 +0200

    :tada: Initial Commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,44 @@
+/* 
+ * Copyright 2019 mon-mode - 0mon.mode@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author mon-mode
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        final String SALUDO_CONSTANTE = "¡Hola Mundo!";
+        
+        //Added another different output 
+        String saludoConstante;
+        saludoConstante = "Hola";
+        
+        System.out.println(saludoConstante);
+        saludoConstante = "Adios";
+        
+
+        // Mensaje
+        System.out.println(SALUDO_CONSTANTE);
+        System.out.println(saludoConstante);
+        
+
+        
+
+    }
+}
